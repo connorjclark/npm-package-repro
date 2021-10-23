fccf3d46d218655cb11c1697f042cdef5261bb50
Connor Clark <cjamcl@gmail.com>
2021-10-23 02:15:23 -0700

    --check-all-deps

--- src/main.js
+++ src/main.js
@@ -6,19 +6,21 @@ import { execFileSync } from 'child_process';
  * @typedef PackageDetails
  * @property {string} name
  * @property {string} version
- * @property {string} gitHead
+ * @property {string=} gitHead
  * @property {Record<string, string>} scripts
  * @property {{type: string, url: string}} repository
  * @property {{integrity: string, shasum: string, tarball: string}} dist
  */
 
 /**
- * @param {string} packageName
+ * @param {string} packageIdentifier
  * @return {PackageDetails}
  */
-function npmView(packageName) {
-  const output = execFileSync('npm', ['view', '--json', packageName], { encoding: 'utf-8' });
-  return JSON.parse(output);
+function npmView(packageIdentifier) {
+  const output = execFileSync('npm', ['view', '--json', packageIdentifier], { encoding: 'utf-8' });
+  const packageDetails = JSON.parse(output);
+  packageDetails.scripts = packageDetails.scripts || {};
+  return packageDetails;
 }
 
 /**
@@ -43,9 +45,33 @@ function gitRevisionExists(dir, rev) {
 
 /**
  * @param {PackageDetails} packageDetails
- * @param {string=} version
  */
-async function processPackage(packageDetails, version) {
+async function processPackageIfNeeded(packageDetails) {
+  const resultPath = `.tmp/results/${packageDetails.name.replace('/', '_')}@${packageDetails.version}.json`;
+  if (fs.existsSync(resultPath)) {
+    return JSON.parse(fs.readFileSync(resultPath, 'utf-8'));
+  }
+
+  console.log('processing:', packageDetails.name, packageDetails.version);
+
+  let result;
+  try {
+    result = await processPackage(packageDetails);
+  } catch (err) {
+    result = {
+      success: false,
+      errors: [err.toString()],
+    };
+  }
+
+  fs.writeFileSync(resultPath, JSON.stringify(result, null, 2));
+  return result;
+}
+
+/**
+ * @param {PackageDetails} packageDetails
+ */
+async function processPackage(packageDetails) {
   if (!packageDetails.repository) {
     return {
       success: false,
@@ -64,8 +90,7 @@ async function processPackage(packageDetails, version) {
     };
   }
 
-  if (!version) version = packageDetails.version;
-
+  const version = packageDetails.version;
   const errors = [];
 
   // Clone + checkout repo.
@@ -73,17 +98,20 @@ async function processPackage(packageDetails, version) {
   fs.mkdirSync(repoDir, { recursive: true });
   if (!fs.existsSync(`${repoDir}/.git`)) {
     const url = packageDetails.repository.url.replace(/^git\+/, '');
-    execFileSync('git', ['clone', url, repoDir]);
+    // TODO: wrap all commands in function that only prints output if the command fails.
+    execFileSync('git', ['clone', url, repoDir], { stdio: 'ignore' });
   }
   const pkgManager = fs.existsSync(`${repoDir}/yarn.lock`) ? 'yarn' : 'npm';
 
-  const possibleCommits = [packageDetails.gitHead, `v${version}`, version];
+  const possibleCommits = [];
+  if (packageDetails.gitHead) possibleCommits.push(packageDetails.gitHead);
+  possibleCommits.push(`v${version}`, version);
   const commit = possibleCommits.find(commit => gitRevisionExists(repoDir, commit));
   if (commit !== packageDetails.gitHead) {
     errors.push(`package was published to npm using unreachable git commit: ${packageDetails.gitHead}. Will try tags.`);
   }
   if (commit) {
-    execFileSync('git', ['checkout', commit], { cwd: repoDir });
+    execFileSync('git', ['checkout', commit], { cwd: repoDir, stdio: 'ignore' });
   } else {
     errors.push(`could not find any relevant commits, tried: ${possibleCommits.join(' ')}`);
     return {
@@ -92,13 +120,13 @@ async function processPackage(packageDetails, version) {
     };
   }
 
-  execFileSync('git', ['clean', '-fxd'], { cwd: repoDir });
+  execFileSync('git', ['clean', '-fxd'], { cwd: repoDir, stdio: 'ignore' });
 
   // Download archive.
   fs.mkdirSync('.tmp/packages', { recursive: true });
   const packageTarballPath = `.tmp/packages/${packageDetails.name}-${version}.tgz`;
   if (!fs.existsSync(packageTarballPath)) {
-    execFileSync('curl', ['-o', packageTarballPath, packageDetails.dist.tarball]);
+    execFileSync('curl', ['-o', packageTarballPath, packageDetails.dist.tarball], { stdio: 'ignore' });
   }
 
   // Unzip archive.
@@ -123,6 +151,13 @@ async function processPackage(packageDetails, version) {
       }
     } else {
       execFileSync('yarn', [], { cwd: repoDir });
+      if (packageDetails.scripts.prepublishOnly) {
+        try {
+          execFileSync('yarn', ['prepublishOnly'], { cwd: repoDir });
+        } catch (err) {
+          errors.push(err.toString());
+        }
+      }
     }
   }
 
@@ -191,7 +226,6 @@ async function processPackage(packageDetails, version) {
     '-c',
     `diff -urNq ${packageDir} ${githubArchiveUnpackedDir}/package || true`,
   ], { encoding: 'utf-8' });
-  console.log({filesDiffRaw: `diff -urNq ${packageDir} ${githubArchiveUnpackedDir}/package || true`});
   const files = filesDiffRaw
     .split('\n')
     .filter(Boolean)
@@ -236,30 +270,52 @@ async function processPackage(packageDetails, version) {
   };
 }
 
-async function main() {
-  const packageNames = [
-    // 'lighthouse',
-    // 'rimraf',
-    // 'yargs',
-    // 'which',
-    // 'isexe',
-    // 'rollup',
-    // 'mime-types',
-    // 'node-fetch',
-    // 'npm-run-posix-or-windows',
-    // 'package-json-versionify',
-    // 'pako',
-    // 'preact',
-    // 'webtreemap-cdt',
-    // 'typed-query-selector',
-    // 'pretty-json-stringify',
-    'es-main',
-  ];
+/**
+ * @param {string} packageIdentifier
+ * @return {Array<{packageName: string, version: string}>}
+ */
+function getPackageDependencies(packageIdentifier) {
+  // --flatten would be nice but for some silly reason the output is truncated at ~100 items.
+  const output = execFileSync('bash', [
+    '-c',
+    `npx --yes npm-remote-ls ${packageIdentifier} -d false -o false`,
+  ], { encoding: 'utf-8' });
+  return output.split('\n').slice(1).map(line => {
+    const [, scope, name, version] = line.match(/─ (@?)(.+)@(.+)/) || [];
+    return { packageName: scope + name, version };
+  });
+}
 
-  for (const packageName of packageNames) {
-    const packageDetails = npmView(packageName);
-    console.log('processing:', packageDetails.name);
-    const result = await processPackage(packageDetails);
+/**
+ * @param {string} packageIdentifier
+ */
+async function checkAllDepsForPackage(packageIdentifier) {
+  const deps = getPackageDependencies(packageIdentifier);
+  for (const { packageName, version } of deps) {
+    const packageDetails = npmView(`${packageName}@${version}`);
+    const result = await processPackageIfNeeded(packageDetails);
+    if (!result.success) console.log(`problematic: ${packageName}@${version}`);
+    else console.log(`ok: ${packageName}@${version}`);
+  }
+}
+
+async function main() {
+  fs.mkdirSync('.tmp/packages-from-source', { recursive: true });
+  fs.mkdirSync('.tmp/packages', { recursive: true });
+  fs.mkdirSync('.tmp/repos', { recursive: true });
+  fs.mkdirSync('.tmp/results', { recursive: true });
+
+  const packageIdentifier = process.argv.slice(2).find(arg => !arg.startsWith('--'));
+  if (!packageIdentifier) throw new Error('missing package identifier');
+
+  // TODO: --check-deps (only those listed in package.json)
+  // TODO: --check-deps-from-source-maps (only those listed in published sm files)
+
+  if (process.argv.includes('--check-all-deps')) {
+    await checkAllDepsForPackage(packageIdentifier);
+  } else {
+    const packageDetails = npmView(packageIdentifier);
+    const result = await processPackageIfNeeded(packageDetails);
     console.log(result);
   }
 }
